319c0a4ff0670e1d86a086c7050ddfe090dbf927
SPOCDigital <107679519+SPOCDigital@users.noreply.github.com>
2025-12-02 13:35:24 +0530

    Add files via upload

---
+++ Lux Dec 1/mac/javascript/app.js
@@ -0,0 +1,11 @@
+// Service Worker disabled - causing scope errors
+// if ("serviceWorker" in navigator) {
+//   navigator.serviceWorker
+//     .register("sw.js", { scope: "./" })
+//     .then(function (reg) {
+//       console.log("Registration succeeded. Scope is " + reg.scope);
+//     })
+//     .catch(function (error) {
+//       console.log("Registration failed with " + error);
+//     });
+// }
